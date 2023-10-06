aa7fbfd4e70fb9e012c34aa19cce5679a60bef15
Jonatan <jonatanare.dev@gmail.com>
2023-10-06 17:48:46 -0500

    create page for edit client

---
+++ pages/clientes/editar/[id].js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../../components/Layout'
+
+const EditarCliente = () => {
+    return (
+        <Layout title={'Editar Cliente'}>
+            <h1 className='text-2xl text-gray-800 font-light'>Clientes</h1>
+        </Layout>
+    )
+}
+
+export default EditarCliente
